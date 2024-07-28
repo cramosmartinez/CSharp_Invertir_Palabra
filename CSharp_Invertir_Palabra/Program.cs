ea93cdd57c8afee72f18ef0e272799307bf63203
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-07-28 10:31:33 -0600

    Agregar archivos de proyecto.

---
+++ CSharp_Invertir_Palabra/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Por favor, introduce una palabra: ");
+        string palabra = Console.ReadLine();
+
+        // Convertir la palabra en un array de caracteres, invertir el array, y crear una nueva cadena
+        char[] caracteres = palabra.ToCharArray();
+        Array.Reverse(caracteres);
+        string palabraInvertida = new string(caracteres);
+
+        Console.WriteLine("Palabra invertida: " + palabraInvertida);
+    }
+}
